bc9d913d3a3d29d33dbe06b4f884fc0042e70af8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-07 18:50:06 +0530

    pass the title property of the blog parameter field

--- src/Home.js
+++ src/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
             {/* <h2>Homepage</h2> */}
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
-                    <h2></h2>
+                    <h2>{blog.title}</h2>
                 </div>
 
             ))}
