995e04b9d6177e5439af71e8db929db9484c2ed3
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-07 18:36:04 +0530

    comment out some part of the code

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 const Home = () => {
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
+            {/* <h2>Homepage</h2> */}
         </div>
      );
 }
