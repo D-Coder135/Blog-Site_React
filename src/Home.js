4ac5d1484de2b9643a0f9803682f00f0b99a6e49
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-07 19:16:48 +0530

    cut some part of the code from Home.js

--- src/Home.js
+++ src/Home.js
@@ -11,13 +11,7 @@ const Home = () => {
     return ( 
         <div className="home">
             {/* <h2>Homepage</h2> */}
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p> Written by { blog.author }</p>
-                </div>
-
-            ))}
+            
         </div>
      );
 }
