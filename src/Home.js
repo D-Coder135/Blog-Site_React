18888b50e6e7dba090a45504e01e27e4ad461d6f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-07 18:39:12 +0530

    initialize a field along with a function with the method call of the useState() function along with some contents inside it as the parameter

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,13 @@
 import { useState } from "react";
 
 const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        {title: 'My new website', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quos, hic, laudantium, cumque odit maxime quo facere quis sapiente impedit harum! Eos est labore sunt quos laborum minus illum ducimus.', author: 'Devansh', id: 1},
+        {title: 'Welcome Party!', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quos, hic, laudantium, cumque odit maxime quo facere quis sapiente impedit harum! Eos est labore sunt quos laborum minus illum ducimus.', author: 'Matilda', id: 2},
+        {title: 'Web dev top tips', body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum quos, hic, laudantium, cumque odit maxime quo facere quis sapiente impedit harum! Eos est labore sunt quos laborum minus illum ducimus.', author: 'John', id: 1},
+    ]);
+
     return ( 
         <div className="home">
             {/* <h2>Homepage</h2> */}
