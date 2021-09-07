20a64b8ddfb95319e205f142785a988032b10307
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-07 19:18:02 +0530

    paste the cut out part of the code from Home.js file inside the div tag

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,6 +1,14 @@
 const BlogList = () => {
     return ( 
-        <div className="blog-list"></div>
+        <div className="blog-list">
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p> Written by { blog.author }</p>
+                </div>
+
+            ))}
+        </div>
      );
 }
  
