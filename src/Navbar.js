0365cf3b35f07f13bbd02801cc9d650652b227ba
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-07 16:08:48 +0530

    add an a tag inside the div tag

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,9 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>Blog Site</h1>
-            <div className="links"></div>
+            <div className="links">
+                <a></a>
+            </div>
         </nav>
      );
 }
