6d35fbe6e57c1dde54193b731f9c4197f6d28f94
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-07 18:35:53 +0530

    remove some part of the code

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,9 @@
 import { useState } from "react";
 
 const Home = () => {
-
-    const[name, setName] = useState('Devansh');
-    const[age, setAge] = useState(21);
-
-    const handleClick = () => {
-        setName('Harshit');
-        setAge(51);
-    }
-
     return ( 
         <div className="home">
             <h2>Homepage</h2>
-            <p>{name} is {age} years old.</p>
-            <button onClick = {handleClick}>Click Me</button>
         </div>
      );
 }
