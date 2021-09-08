e1118ef45e8bcfc5d1092dc573371b928d4f7971
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-08 16:47:06 +0530

    add an h1 tag inside the return statement

--- src/BlogList.js
+++ src/BlogList.js
@@ -3,6 +3,7 @@ const BlogList = ({ blogs, title}) => {
     // const blogs = props.blogs;
 
     return ( 
+        <h1></h1>
         <div className="blog-list">
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
