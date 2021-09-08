71461e32648a387dfa45f9ed250ffec7f2b13170
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-08 16:51:53 +0530

    add some fix up to the code

--- src/BlogList.js
+++ src/BlogList.js
@@ -4,7 +4,7 @@ const BlogList = ({ blogs, title}) => {
 
     return ( 
         <div className="blog-list">
-            <h1>{ title }</h1>
+            <h2>{ title }</h2>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
