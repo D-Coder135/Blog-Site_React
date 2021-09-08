4ff8af775b786873e02cb36a4aab1d793ab0ecb8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-08 16:59:22 +0530

    add some fix up to the code

--- src/Home.js
+++ src/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
     ]);
 
     const handleDelete = (id) => {
-        const newBlogs = blogs.filter(() => blogs.id != id);
+        const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
