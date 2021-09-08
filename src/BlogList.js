5856f9846681f70b2560edf4ed9b55c75f1c24df
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-08 16:57:13 +0530

    add the onClick property for the button tag

--- src/BlogList.js
+++ src/BlogList.js
@@ -9,7 +9,7 @@ const BlogList = ({ blogs, title}) => {
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p> Written by { blog.author }</p>
-                    <button>Delete Blog</button>
+                    <button onClick = {() => handleDelete(blog.id)}>Delete Blog</button>
                 </div>
 
             ))}
