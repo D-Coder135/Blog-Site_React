045f5dcc0563e019fdfe9486c880e900beff2169
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-09 16:51:22 +0530

    comment out some part of the code

--- src/Home.js
+++ src/Home.js
@@ -5,10 +5,10 @@ const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
+    // const handleDelete = (id) => {
+    //     const newBlogs = blogs.filter(blog => blog.id !== id);
+    //     setBlogs(newBlogs);
+    // }
 
     useEffect(() => {
         fetch('http://localhost:8000/blogs').then(res => {
