d88c6e8e7eebcc322f44ac0cc47f46ee5099ab0e
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-10 23:05:54 +0530

    call the Error() method along with the throw keyword

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs').then(res => {
                 if(!res.ok) {
-                    
+                    throw Error('Could Not Fetch The Data For That Response!');
                 }
             return res.json();
         }).then(data => {
