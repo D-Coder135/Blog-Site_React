e9b0842a869159ef1aa4fc26d6e59215c64bbb21
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-10 23:12:59 +0530

    call the setError() method inside the catch() method

--- src/Home.js
+++ src/Home.js
@@ -24,6 +24,7 @@ const Home = () => {
             setIsLoading(false);
         }).catch(error => {
             // console.log(error.message);
+            setError(error.message);
         })
         }, 1000);
     }, []);
