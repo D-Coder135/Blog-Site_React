50657df749a632e783541341dc8fd63673b39ff4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-11 11:27:02 +0530

    paste the cut out part of the code inside the useFetch.js

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,3 +1,20 @@
 const useFetch = () => {
-    
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs').then(res => {
+                if(!res.ok) {
+                    throw Error('Could Not Fetch The Data For That Response!');
+                }
+            return res.json();
+        }).then(data => {
+            setBlogs(data);
+            setIsLoading(false);
+            setError(null);
+        }).catch(error => {
+            // console.log(error.message);
+            setError(error.message);
+            setIsLoading(false);
+        })
+        }, 1000);
+    }, []);
 }
