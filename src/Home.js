ff69edb1ae64c78c2565731230c5cac7a6041545
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-11 11:51:16 +0530

    add some fix up

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ import useFetch from "./useFetch";
 
 const Home = () => {
 
-    const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs';)
+    const {data: blogs, isLoading, error} = useFetch('http://localhost:8000/blogs');
 
     // const handleDelete = (id) => {
     //     const newBlogs = blogs.filter(blog => blog.id !== id);
