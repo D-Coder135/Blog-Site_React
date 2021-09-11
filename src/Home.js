b062cd2f942015fea1d0daf447e7a488794be750
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-09-11 11:26:45 +0530

    cut some part of the code from Home.js file

--- src/Home.js
+++ src/Home.js
@@ -12,24 +12,7 @@ const Home = () => {
     //     setBlogs(newBlogs);
     // }
 
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs').then(res => {
-                if(!res.ok) {
-                    throw Error('Could Not Fetch The Data For That Response!');
-                }
-            return res.json();
-        }).then(data => {
-            setBlogs(data);
-            setIsLoading(false);
-            setError(null);
-        }).catch(error => {
-            // console.log(error.message);
-            setError(error.message);
-            setIsLoading(false);
-        })
-        }, 1000);
-    }, []);
+    
 
     return ( 
         <div className="home">
